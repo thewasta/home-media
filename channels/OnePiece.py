b394af56f339368e5d33acc9b9eb723c552cda8a
schenier.lopez <6Jn=P*tfQP7dBt&w>
2022-01-17 19:29:26 +0100

    FIX ignore medias

--- channels/OnePiece.py
+++ channels/OnePiece.py
@@ -19,7 +19,7 @@ class OnePiece(ChannelFactory):
         return "3D" in message.message or "Película" in message.message
 
     def get_path(self, message: Message):
-        if self.must_ignore(message):
+        if not self.must_ignore(message):
             file_type = FileMimeType.get_mime(message.media.document.mime_type)
             main_folder_path = PurePath(str(config['Telegram']['PATH']), self.parent, "TV", self.show)
             if "Cap" in message.message:
